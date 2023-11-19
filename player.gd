dcfe86b14f98ee58fd2b363ffca8be6f0c057223
Erik Mejer Hansen <erik.m.hansen@maersk.com>
2023-11-19 21:16:56 +0100

    arms track mouse

---
+++ player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const ROTATE_SPEED = 0.7
+
+var rotate_clockwise = false
+var rotate_counter_clockwise = false
+
+func _physics_process(delta):
+	if(rotate_clockwise):
+		rotation += ROTATE_SPEED * delta
+		rotate_clockwise = false
+	
+	if(rotate_counter_clockwise):
+		rotation -= ROTATE_SPEED * delta
+		rotate_counter_clockwise = false
+
+
+
+func _on_arm_rotation_clamp(clockwise):
+	if(clockwise):
+		rotate_clockwise = true
+	else:
+		rotate_counter_clockwise = true
---
+++ right_arm.gd
@@ -0,0 +1,20 @@
+extends Sprite2D
+signal arm_rotation_clamp(clockwise)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	look_at(get_global_mouse_position())
+	var clamped_rotation = clampf(rotation, deg_to_rad(-35), deg_to_rad(45))
+	
+	if(clamped_rotation < rotation):
+		arm_rotation_clamp.emit(true)
+	
+	if (clamped_rotation > rotation):
+		arm_rotation_clamp.emit(false)
+	
+	rotation = clamped_rotation
