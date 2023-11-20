9bf79eb92dad8923ba52739a300db5688813fd40
Erik Mejer Hansen <erik.m.hansen@maersk.com>
2023-11-20 14:58:43 +0100

    add simple wasd movement

--- player.gd
+++ player.gd
@@ -8,6 +8,7 @@ var rotate_clockwise = false
 var rotate_counter_clockwise = false
 
 func _physics_process(delta):
+	
 	if(rotate_clockwise):
 		rotation += ROTATE_SPEED * delta
 		rotate_clockwise = false
@@ -15,7 +16,13 @@ func _physics_process(delta):
 	if(rotate_counter_clockwise):
 		rotation -= ROTATE_SPEED * delta
 		rotate_counter_clockwise = false
+		
+	var input_direction = Input.get_vector("backward", "forward", "left", "right")
+	var movement_direction = input_direction.rotated(rotation)
 
+	velocity = movement_direction * SPEED
+	
+	move_and_slide()
 
 
 func _on_arm_rotation_clamp(clockwise):
