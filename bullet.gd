48ab640447154734c878d699bdf87c19d12f6c89
Erik Mejer Hansen <erik.m.hansen@maersk.com>
2023-11-26 21:31:11 +0100

    feat: sparks fly when hard surfaces are hit

--- bullet.gd
+++ bullet.gd
@@ -3,6 +3,8 @@ extends Area2D
 
 @export var damage:int = 10
 @export var SPEED:int = 1300
+
+var sparks = preload("res://effects/hard_surface_sparks.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -25,5 +27,19 @@ func _on_area_entered(area):
 
 
 func _on_body_entered(body):
+	if _hit_hard_surface(body):
+		show_sparks()
+		
+		
 	if not body.is_in_group("player"):
 		queue_free()
+	
+func _hit_hard_surface(body):
+	return body is TileMap
+
+func show_sparks():
+	var s = sparks.instantiate()
+	s.global_position = global_position
+	s.rotation_degrees = rotation_degrees - 180
+	get_tree().root.add_child(s)
+	s.restart()
